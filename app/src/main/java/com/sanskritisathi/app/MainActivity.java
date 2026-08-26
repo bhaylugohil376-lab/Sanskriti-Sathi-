ed4c7fe45210393fcad77e31fa673fab808b9df3
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-27 00:16:28 +0530

    Fix button ID reference in MainActivity

--- app/src/main/java/com/sanskritisathi/app/MainActivity.java
+++ app/src/main/java/com/sanskritisathi/app/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Button rajaButton = findViewById(R.id.btnRaja);
+        Button rajaButton = findViewById(R.id.rajaButton);
 
         rajaButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RajaActivity.class);
