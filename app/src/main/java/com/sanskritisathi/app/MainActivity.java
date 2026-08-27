05f0f19b88a17049e0d61d297a979316c3860153
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-27 15:04:44 +0530

    Add button click listener to launch RajaActivity

--- app/src/main/java/com/sanskritisathi/app/MainActivity.java
+++ app/src/main/java/com/sanskritisathi/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sanskritisathi.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rajaButton = findViewById(R.id.rajaButton);
+
+        rajaButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RajaActivity.class);
+            startActivity(intent);
+        });
     }
 }
