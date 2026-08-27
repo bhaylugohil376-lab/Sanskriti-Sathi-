ec321c435468bbe4e9ac1069e60db250d4150509
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-27 23:25:42 +0530

    Create Temple class with attributes and methods

---
+++ app/src/main/java/com/sanskritisathi/app/Temple.java
@@ -0,0 +1,26 @@
+package com.sanskritisathi.app;
+
+public class Temple {
+
+    private String name;
+    private String description;
+    private int imageResId;
+
+    public Temple(String name, String description, int imageResId) {
+        this.name = name;
+        this.description = description;
+        this.imageResId = imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+}
