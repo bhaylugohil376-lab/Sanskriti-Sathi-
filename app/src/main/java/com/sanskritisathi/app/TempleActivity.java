4e216ec2662229037d6d3c9de4831e12d7d1d6ac
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-27 23:48:48 +0530

    Refactor TempleActivity to use XML layout

--- app/src/main/java/com/sanskritisathi/app/TempleActivity.java
+++ app/src/main/java/com/sanskritisathi/app/TempleActivity.java
@@ -1,7 +1,6 @@
 package com.sanskritisathi.app;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,18 +10,6 @@ public class TempleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textView = new TextView(this);
-
-        textView.setText(
-                "🛕 Sanskriti Sathi\n\n" +
-                "Mandir aur Dharmik Sthal\n\n" +
-                "Yahan Bharat ke prasiddh mandiron, " +
-                "devi-devtaon aur dharmik sthalon ki jankari milegi."
-        );
-
-        textView.setTextSize(20);
-        textView.setPadding(40, 60, 40, 40);
-
-        setContentView(textView);
+        setContentView(R.layout.activity_temple);
     }
 }
