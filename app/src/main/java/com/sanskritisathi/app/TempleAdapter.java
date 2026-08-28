3ea21c4a70044d81b571f583d5175560903a3bc4
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-28 15:25:28 +0530

    Refactor TempleAdapter and add location field
    Refactor TempleAdapter for improved readability and add location display.

--- app/src/main/java/com/sanskritisathi/app/TempleAdapter.java
+++ app/src/main/java/com/sanskritisathi/app/TempleAdapter.java
@@ -7,22 +7,30 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class TempleAdapter extends RecyclerView.Adapter<TempleAdapter.TempleViewHolder> {
+public class TempleAdapter
+        extends RecyclerView.Adapter<TempleAdapter.TempleViewHolder> {
 
-    private Context context;
-    private ArrayList<Temple> templeList;
+    private final Context context;
+    private final ArrayList<Temple> templeList;
 
-    public TempleAdapter(Context context, ArrayList<Temple> templeList) {
+    public TempleAdapter(
+            Context context,
+            ArrayList<Temple> templeList) {
+
         this.context = context;
         this.templeList = templeList;
     }
 
+    @NonNull
     @Override
-    public TempleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TempleViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
 
         View view = LayoutInflater.from(context)
                 .inflate(R.layout.item_temple, parent, false);
@@ -31,13 +39,23 @@ public class TempleAdapter extends RecyclerView.Adapter<TempleAdapter.TempleView
     }
 
     @Override
-    public void onBindViewHolder(TempleViewHolder holder, int position) {
+    public void onBindViewHolder(
+            @NonNull TempleViewHolder holder,
+            int position) {
 
         Temple temple = templeList.get(position);
 
-        holder.templeImage.setImageResource(temple.getImageResId());
-        holder.templeName.setText(temple.getName());
-        holder.templeDescription.setText(temple.getDescription());
+        holder.templeImage.setImageResource(
+                temple.getImageResId());
+
+        holder.templeName.setText(
+                temple.getName());
+
+        holder.templeLocation.setText(
+                "📍 " + temple.getLocation());
+
+        holder.templeDescription.setText(
+                temple.getDescription());
     }
 
     @Override
@@ -45,18 +63,28 @@ public class TempleAdapter extends RecyclerView.Adapter<TempleAdapter.TempleView
         return templeList.size();
     }
 
-    static class TempleViewHolder extends RecyclerView.ViewHolder {
+    public static class TempleViewHolder
+            extends RecyclerView.ViewHolder {
 
         ImageView templeImage;
         TextView templeName;
+        TextView templeLocation;
         TextView templeDescription;
 
-        public TempleViewHolder(View itemView) {
+        public TempleViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            templeImage = itemView.findViewById(R.id.templeImage);
-            templeName = itemView.findViewById(R.id.templeName);
-            templeDescription = itemView.findViewById(R.id.templeDescription);
+            templeImage = itemView.findViewById(
+                    R.id.templeImage);
+
+            templeName = itemView.findViewById(
+                    R.id.templeName);
+
+            templeLocation = itemView.findViewById(
+                    R.id.templeLocation);
+
+            templeDescription = itemView.findViewById(
+                    R.id.templeDescription);
         }
     }
 }
