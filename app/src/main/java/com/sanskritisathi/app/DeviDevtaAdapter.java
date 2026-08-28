54fbab4e45a9edd90ff9c13f007d11bcc27055da
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-28 14:59:35 +0530

    Implement DeviDevtaAdapter for RecyclerView

---
+++ app/src/main/java/com/sanskritisathi/app/DeviDevtaAdapter.java
@@ -0,0 +1,83 @@
+package com.sanskritisathi.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DeviDevtaAdapter
+        extends RecyclerView.Adapter<DeviDevtaAdapter.DeviDevtaViewHolder> {
+
+    private final Context context;
+    private final ArrayList<DeviDevta> deviDevtaList;
+
+    public DeviDevtaAdapter(
+            Context context,
+            ArrayList<DeviDevta> deviDevtaList) {
+
+        this.context = context;
+        this.deviDevtaList = deviDevtaList;
+    }
+
+    @NonNull
+    @Override
+    public DeviDevtaViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
+
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.item_devi_devta, parent, false);
+
+        return new DeviDevtaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull DeviDevtaViewHolder holder,
+            int position) {
+
+        DeviDevta deviDevta = deviDevtaList.get(position);
+
+        holder.deviDevtaImage.setImageResource(
+                deviDevta.getImageResId());
+
+        holder.deviDevtaName.setText(
+                deviDevta.getName());
+
+        holder.deviDevtaDescription.setText(
+                deviDevta.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return deviDevtaList.size();
+    }
+
+    public static class DeviDevtaViewHolder
+            extends RecyclerView.ViewHolder {
+
+        ImageView deviDevtaImage;
+        TextView deviDevtaName;
+        TextView deviDevtaDescription;
+
+        public DeviDevtaViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            deviDevtaImage = itemView.findViewById(
+                    R.id.deviDevtaImage);
+
+            deviDevtaName = itemView.findViewById(
+                    R.id.deviDevtaName);
+
+            deviDevtaDescription = itemView.findViewById(
+                    R.id.deviDevtaDescription);
+        }
+    }
+}
