6775357a8b2aaafc02b7d5701d4a834339113f82
bhaylugohil376-lab <bhaylugohil376@gmail.com>
2026-08-30 15:06:17 +0530

    Add pubDate field to RssItem class

--- app/src/main/java/com/sanskritisathi/app/RssItem.java
+++ app/src/main/java/com/sanskritisathi/app/RssItem.java
@@ -5,11 +5,18 @@ public class RssItem {
     private final String title;
     private final String description;
     private final String link;
+    private final String pubDate;
 
-    public RssItem(String title, String description, String link) {
+    public RssItem(
+            String title,
+            String description,
+            String link,
+            String pubDate) {
+
         this.title = title;
         this.description = description;
         this.link = link;
+        this.pubDate = pubDate;
     }
 
     public String getTitle() {
@@ -22,5 +29,9 @@ public class RssItem {
 
     public String getLink() {
         return link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
     }
 }
